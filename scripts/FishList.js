a3850864545e2aa0d8331159454d4dac86cd764b
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-07 15:29:41 -0500

    Completed the function so now fish objects render to the page

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,12 +1,17 @@
 import {useFish} from "./FishDataProvider.js"
+import {Fish} from "./fish.js"
 
 export const FishList = () => {
     const contentElement = document.querySelector(".fish")
     const fishes = useFish()
 
+    let fishHTMLRepresentations = ""
+    for (const fishy of fishes) {
+        fishHTMLRepresentations += Fish(fishy)
+    }
+
     contentElement.innerHTML += `
-    <article class="fishList">
-            All the fish go here!
-        </article>
+            ${fishHTMLRepresentations}
+        
         `
 }
