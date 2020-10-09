af1aacda19e0666f997d2e5c6bffaff3979bf2be
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-09 13:43:17 -0500

    created functions to provide lists of fish that are holy, unholy, or soldiers

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -2,7 +2,7 @@ const fishCollection = [
     {
         name: "Hercules",
         species: "Discus",
-        length: "12-15 cm",
+        length: 12,
         harvest_location: "Amazon River Delta",
         diet: "Algae flakes & shrimp pellets",
         image: "https://images.unsplash.com/photo-1548425923-a7d6c37d7f18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
@@ -10,7 +10,7 @@ const fishCollection = [
     {
         name: "Hercules",
         species: "Discus",
-        length: "12-15 cm",
+        length: 12,
         harvest_location: "Amazon River Delta",
         diet: "Algae flakes & shrimp pellets",
         image: "https://images.unsplash.com/photo-1548425923-a7d6c37d7f18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
@@ -19,7 +19,7 @@ const fishCollection = [
     {
         name: "Shadow",
         species: "Black Ghost Knifefish",
-        length: "50 cm",
+        length: 50,
         harvest_location: "Paraguay-Parana River",
         diet: "Live worms",
         image: "https://www.petlandtexas.com/wp-content/uploads/2020/01/Petland_Texas_Black_Ghost_Knifefish.jpg"
@@ -27,13 +27,50 @@ const fishCollection = [
     {
         name: "Shadow",
         species: "Black Ghost Knifefish",
-        length: "50 cm",
+        length: 50,
         harvest_location: "Paraguay-Parana River",
         diet: "Live worms",
         image: "https://www.petlandtexas.com/wp-content/uploads/2020/01/Petland_Texas_Black_Ghost_Knifefish.jpg"
     },
 ]
 
+// Creates a copy of the first array, which is now a new array
 export const useFish = () => {
     return fishCollection.slice()
+}
+
+// This gets me a list of fish whose length is divisible by 3
+export const holyFish = () => {
+    let holyList = []    
+    for (const fish of fishCollection) {
+        if (fish.length % 3 === 0) {
+            holyList.push(fish)
+        }
+    }
+    return holyList
+}
+
+// This gets me a list of fish whose length is divisible by 5 but NOT by 3
+export const soldierFish = () => {
+    debugger
+    let soldierList = []
+    for (const fish of fishCollection) {
+        if (fish.length % 5 === 0 && fish.length % 3 !== 0) {
+            soldierList.push(fish)
+        }
+    }
+    console.log(soldierList)
+    return soldierList
+}
+
+// The gets me a list of fish that are not divisible by either 3 or 5
+export const unHolyFish = () => {
+    let unHolyList = []
+    for (const fish of fishCollection) {
+        if (fish.length % 3 !==0 && fish.length % 5 !== 0) {
+            unHolyList.push(fish)
+        }
+    }
+    console.log("probably nothing will go here")
+    return unHolyList
 }
--- scripts/fish/fish.js
+++ scripts/fish/fish.js
@@ -1,4 +1,4 @@
-// Renders individual fish objects
+// Renders individual fish objects in HTML
 
 export const Fish = (fishObj) => {
     return `
@@ -6,7 +6,7 @@ export const Fish = (fishObj) => {
         <div class="fish__item">
             <img src=${fishObj.image}>
             <p>Species: ${fishObj.species}</p>
-            <p>Length: ${fishObj.length}</p>
+            <p>Length: ${fishObj.length} cm </p>
             <p>Name: ${fishObj.name}</p>
             <p>Harvest Location: ${fishObj.harvest_location}</p>
             <p>Diet: ${fishObj.diet} </p>
